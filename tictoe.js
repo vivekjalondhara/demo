e6b91dd6f1ff3f970b2f4019a8b69a84732e3d6f
vivek <vivekjalondhara1103@gmail.com>
2023-03-14 08:24:27 +0530

    changes

---
+++ tictoe.js
@@ -0,0 +1,39 @@
+function mytictacfunction(){
+var vivekj1 = document.getElementById("cell1")
+var vivekj2 = document.getElementById("cell2")
+var vivekj3 = document.getElementById("cell3")
+var vivekj4 = document.getElementById("cell4")
+var vivekj5 = document.getElementById("cell5")
+var vivekj6 = document.getElementById("cell6")
+var vivekj7 = document.getElementById("cell7")
+var vivekj8 = document.getElementById("cell8")
+var vivekj9 = document.getElementById("cell9")
+function click1cell(){
+    
+}
+function click2cell(){
+
+}
+function click3cell(){
+
+}
+function click4cell(){
+
+}
+function click5cell(){
+
+}
+function click6cell(){
+
+}
+function click7cell(){
+gg
+}
+function click8cell()
+{
+
+}
+function click9cell(){
+
+}
+}
